3c9dbeece529fe8f9d47abbcdafeb3ac475b33ab
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2023-03-06 11:47:58 -0800

    image support and documentation

---
+++ README.md
@@ -0,0 +1,48 @@
+# Web Development Project 2 - Flashcards
+
+Submitted by: Samantha Prestrelski
+
+This web app: Need to brush up on your algorithms? Use this study buddy to practice algorithms, proof of correctness outlines, and runtime.
+
+Time spent: **6** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] The title of the card set and some information about it, such as a short description and the total number of cards are displayed
+- [X] A single card at a time is displayed, only showing one of the components of the information pair
+- [X] A list of card pairs is created
+- [X] Clicking on the card shows the corresponding component of the information pair
+- [X] Clicking the next button displays a random new card
+
+The following **optional** features are implemented:
+
+- [X] Cards contains images in addition to or in place of text
+- [X] Cards have different visual styles such as color based on their category
+  - [X] Different colors for algorithms, proof outlines, and runtime.
+
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented required features:
+
+<img src='https://github.com/sprestrelski/flashcards/blob/main/flashcards.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with ezgif.
+
+## License
+
+    Copyright 2023 Samantha Prestrelski
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/App.jsx
+++ src/App.jsx
@@ -8,60 +8,74 @@ const App = () => {
       ques: "What is the runtime of breadth first search (BFS)?",
       ans: "O(V+E)",
       type: "runtime",
+      hasImage: true,
+      image: "/BFS.png",
     },
     {
       id: 1,
       ques: "What is the runtime of depth first search (DFS)?",
       ans: "O(V+E)",
       type: "runtime",
+      hasImage: true,
+      image: "/DFS.png",
     },
     {
       id: 2,
       ques: "What is the proof outline for correctness of divide and conquer algorithms?",
       ans: "We can prove correctness by induction on problem size.\nBase Case: non recursive case.\nInductive step: assuming the smaller recursive calls are correct, show that the algorithm works",
       type: "proof",
+      hasImage: false,
     },
     {
       id: 3,
       ques: "What is the general algorithm technique for divide and conquer algorithms?",
       ans: "1. Break problem into pieces\n2. Solve pieces recursively\n3. Recombine pieces to get answer",
       type: "algorithm",
+      hasImage: true,
+      image: "/DCMergeSort.png",
     },
     {
       id: 4,
       ques: "What is the general algorithm technique for greedy algorithms?",
       ans: "1. Find decision criterion\n2. Make best choice according to criterion\n3. Repeat until done",
       type: "algorithm",
+      hasImage: false,
     },
     {
       id: 5,
       ques: "What is the proof outline for greedy algorithms? Hint: Exchange argument",
       ans: "A greedy algorithm makes a sequence of decisions D_1, D_2,... D_n to eventually reach solution G. Need to show that for arbitrary solutions A that G >= A to prove optimality. Inductively construct this sequence.",
       type: "proof",
+      hasImage: false,
     },
     {
       id: 6,
       ques: "What techniques can be used to construct minimum spanning trees?",
       ans: "Kruskals: When more than one vertex, add lightest edge and merge. Most efficient algorithm uses union finds.\nPrims: Add lightest edge that connects v to a new vertex.",
       type: "algorithm",
+      hasImage: false,
     },
     {
       id: 7,
       ques: "What are the runtimes of Kruskal's and Prim's algorithms?",
       ans: "Kruskal's: O(|E|log|E|)\nPrims: O(|V|log|V| + |E|)",
-      type: "runtime"
+      type: "runtime",
+      hasImage: false,
     },
     {
       id: 8,
       ques: "What is the general algorithm technique for dynamic programming?",
-      ans: "1. Break problem into smaller subproblems\n2. Find a recursive formula solving one subproblem in terms of simpler ones\n3. Tabulate answers and solve all subproblems",
+      ans: "1. Break problem into smaller subproblems\n2. Find a recursive formula solving one subproblem in terms of simpler ones\n3. Tabulate answers and solve all subproblems\n",
       type: "algorithm",
+      hasImage: true,
+      image: "/DPKnapsackExample.png",
     },
     {
       id: 9,
       ques: "What is the proof outline for dynamic programming?",
       ans: "Prove by induction that each table entry is filled out correctly. Use base-case and recursion. The runtime is usually [number of subproblems] x [time per subproblem]",
-      type: "proof"
+      type: "proof",
+      hasImage: false,
     }
   ];
 
@@ -87,9 +101,15 @@ const App = () => {
         <p>total number of cards: {cards.length}</p>
       </div>
       <div className="container">
-        <div className="card" onClick={clickCard}>
+        <div className="card" onClick={clickCard} id={cardText.type}>
         <p>{cardAnswer ? cardText.ans : cardText.ques}</p>
         </div>
+      </div>
+
+      <div className="container">
+      {cardText.hasImage && cardAnswer && 
+          <img src={cardText.image} width="450px"/>
+      }
       </div>
       <div className="container">
         <button onClick={nextCard}>next</button>
